af8733c2a141f411be420e4527bef15aa73fe573
zerolee <zmqclee@gmail.com>
2018-03-08 01:36:52 +0800

    增加 C-x b 绑定

--- Configurations/bindings.el
+++ Configurations/bindings.el
@@ -71,6 +71,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-x")     'counsel-M-x)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
 
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
