6cd8d405d3cc8a4aea917d89f3a9e1f5a59326af
zerolee <zmqclee@gmail.com>
2018-08-23 18:52:13 +0800

    修改 README.org 使其更加符合配置的实际内容
    custom.el 增加 projecile, cousel 以避免 package 的 autoremove

--- Configurations/custom.el
+++ Configurations/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet-snippets wgrep web-mode use-package treemacs solarized-theme smex slime paredit multiple-cursors lsp-ui lsp-java key-chord ivy-xref iedit goto-chg geiser exwm expand-region eglot cquery company-quickhelp company-lsp cmake-mode auto-yasnippet)))
+   '(projectile counsel yasnippet-snippets wgrep web-mode use-package treemacs solarized-theme smex slime paredit multiple-cursors lsp-ui lsp-java key-chord ivy-xref iedit goto-chg geiser exwm expand-region eglot cquery company-quickhelp company-lsp cmake-mode auto-yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
