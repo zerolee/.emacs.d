28642c1532ebe84936e5dc332cbae65c45f1e26e
zerolee <zmqclee@gmail.com>
2020-05-18 05:24:34 +0800

    zerolee-lib.el: 去除 save-position 依赖

--- Extensions/zerolee-lib.el
+++ Extensions/zerolee-lib.el
@@ -18,7 +18,6 @@
 ;;; 一些自己使用的函数
 ;;; Code:
 
-(require 'save-position)
 (require 'cl-lib)
 (defun zerolee-goto-some-window (buffer)
   "去一个指定 buffer 的 window"
@@ -28,16 +27,13 @@
 
 (defun zerolee-delete-some-window (buffer)
   "删除一个指定 buffer 的 window"
-  (if (equal buffer (current-buffer))
+  (let ((bf (current-buffer)))
+    (zerolee-goto-some-window buffer)
+    (when (equal buffer (current-buffer))
       (if (= (count-windows) 1)
           (bury-buffer)
         (delete-window))
-    (sp-push-position-to-ring)
-    (zerolee-goto-some-window buffer)
-    (when (equal buffer (current-buffer))
-      (delete-window)
-      (sp-get-position-from-ring)
-      (sp-push-position-to-ring))))
+      (zerolee-goto-some-window bf))))
 
 (defun zerolee-position-some-window (buffer)
   "查看 buffer 在打开的 window 中的位置，不存在返回 nil"
