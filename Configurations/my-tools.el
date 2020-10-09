c6fbddeb7e804f5fb3b3f2694f27285fb8a81e71
zerolee <zmqclee@gmail.com>
2020-10-09 17:12:15 +0800

    my-tools.el: 梳理代码

--- Configurations/my-tools.el
+++ Configurations/my-tools.el
@@ -17,7 +17,7 @@
 
 ;;; 主要对 Emacs 自带的 [e]shell进行配置
 ;;; Code:
-
+(require 'thingatpt)
 
 (defvar zerolee--eshell-path-hashtable (make-hash-table :test #'equal)
   "每次启动 eshell 的时候将启动时的路径存储进 hash-table 中")
@@ -69,15 +69,14 @@
           (file-name-nondirectory (buffer-file-name))))
 
 (defsubst zerolee--eshell-run (num app)
-  "当 num 为 1 时运行指定程序，否则打开一个新的 gnome-terminal"
-  (let ((default-directory (zerolee--eshell-get-project-root))
-        (height (round (* 0.45 (frame-height))))
+  "当 NUM 为 1 时运行指定程序 APP，否则打开一个新的 gnome-terminal."
+  (let ((height (round (* 0.45 (frame-height))))
         (width (round (+ 1 (frame-width))))
         (left (round (+ 10 (cadar (frame-geometry)))))
         (up (round (+ (cddar (frame-geometry)) (* 0.51 (frame-outer-height))))))
     (shell-command (concat
                     "EMACS_START=nil gnome-terminal --working-directory="
-                    default-directory
+                    (zerolee--eshell-get-project-root)
                     " --geometry="
                     (format "%sx%s+%s+%s" width height left up)
                     (when (= 1 num)
@@ -86,14 +85,13 @@
 
 ;;;###autoload
 (defun zerolee-eshell (&optional num)
-  "若处于 eshell-mode 中则删除该窗口，
-
-   否则的话，获取【正确】的关联目录，关联目录是否关联 eshell，
-   已关联的话直接打开关联的 eshell，否则的话打开一个新的 eshell。
-   存在可运行的程序时，默认打开 gnome-terminal 运行
-   传入参数 2 强制打开 gnome-terminal
-   传入参数 3 强制打开 eshell
-   传入参数 4 强制当前目录启动 eshell "
+  "若处于 `eshell-mode' 中则删除该窗口.
+否则的话，获取【正确】的关联目录，关联目录是否关联 eshell;
+已关联的话直接打开关联的 eshell，否则的话打开一个新的 eshell;
+存在可运行的程序时，默认打开 gnome-terminal 运行;
+NUM 为 2 强制打开 gnome-terminal;
+NUM 为 3 强制打开 eshell;
+NUM 为 4 强制当前目录启动 eshell."
   (interactive "p")
   (require 'eshell)
   (require 'esh-mode)
@@ -106,14 +104,10 @@
            (wn (gethash default-directory zerolee--eshell-path-hashtable))
            (max (gethash "max" zerolee--eshell-path-hashtable))
            (app (when (buffer-file-name) (zerolee--eshell-get-app))))
-      (if (and
-           (or app (= num 2))
-           (< num 3))
+      (if (and (or app (= num 2)) (< num 3))
           (zerolee--eshell-run num app)
         (if wn
-            (let ((buffer
-                   (get-buffer (concat "*eshell*<"
-                                       (number-to-string wn) ">"))))
+            (let ((buffer (get-buffer (format "*eshell*<%s>" wn))))
               (if (and buffer (get-buffer-window buffer))
                   (delete-windows-on buffer)
                 (eshell wn)))
@@ -134,11 +128,11 @@
       (call-interactively #'compile))))
 
 ;;;###autoload
-(defun zerolee-find-file ()
-  "查找文件"
-  (interactive)
+(defun zerolee-find-file (&optional NUM)
+  "查找文件：NUM=1 且存在 .gitignore 时调用 `counsel-git' 否则调用 `counsel-fzf'."
+  (interactive "p")
   (let ((default-directory (zerolee--eshell-get-project-root)))
-    (if (file-readable-p ".gitignore")
+    (if (and (file-readable-p ".gitignore") (= 1 NUM))
         (call-interactively #'counsel-git)
       (call-interactively #'counsel-fzf))))
 
@@ -151,18 +145,15 @@
 
 ;;;###autoload
 (defun zerolee-go ()
-  "综合了 projectile-find-file-dwim zerolee-rg 等的跳转函数"
+  "综合了 `projectile-find-file-dwim' `zerolee-rg' 等的跳转函数."
   (interactive)
   (require 'projectile)
-  (let* ((filename (thing-at-point 'filename t))
-         (suffix (nth 1 (split-string filename "\\."))))
-    (when filename
-      (if suffix
-          (cond ((and (equal major-mode 'c-mode)
-                      (or (string= "h" suffix)
-                          (string= "c" suffix)))
-                 (call-interactively #'projectile-find-file-dwim)))
-        (zerolee-rg (concat "\\b" filename "\\b")))))
+  (if (save-excursion
+        (search-backward "#include" (line-beginning-position) t))
+      (condition-case nil
+          (call-interactively #'projectile-find-file-dwim)
+        (error (call-interactively #'ffap)))
+    (zerolee-rg (concat "\\b" (thing-at-point 'filename t) "\\b")))
   (ove-mode 1))
 
 
@@ -170,33 +161,25 @@
 ;;; 使用外部程序打开相关文件
 ;;;###autoload
 (defun zerolee-open-with (arg)
-  "使用外部程序打开浏览的文件或者当前光标下的链接
-处于 dired mode 时, 打开当前光标下的文件
-处于 org mode 时，若当前光标下存在文件链接，使用外部程序打开链接文件
-使用 prefix ARG 时指定使用的外部程序 "
+  "使用外部程序打开浏览的文件或者当前光标下的链接.
+处于 dired mode 时, 打开当前光标下的文件;
+若当前光标下存在文件链接，使用外部程序打开链接文件;
+使用 prefix ARG 时指定使用的外部程序."
   (interactive "P")
-  (require 'dired)
-  (require 'thingatpt)
   (let ((current-file-name
-         (if (eq major-mode 'dired-mode)
-             (dired-get-file-for-visit)
-           (if (and
-                (eq major-mode 'org-mode)
-                (help-at-pt-string))
-               (let ((current-string (split-string (help-at-pt-string) ":")))
-                 (concat (if (string-match
-                              (string-trim (nth 1 current-string))
-                              "https")
-                             (concat (string-trim (nth 1 current-string)) ":")
-                           nil)
-                         (expand-file-name
-                          (string-trim
-                           (car (last current-string 1))))))
-             (or (thing-at-point 'url) buffer-file-name))))
-        (program
-         (if arg
-             (read-shell-command "Open current file with: ")
-           "xdg-open")))
+         (cond ((eq major-mode 'dired-mode) (dired-get-file-for-visit))
+               ((help-at-pt-string)
+                (let ((current-string
+                       (nth 1 (split-string (help-at-pt-string)))))
+                  (if (string-match "^file:" current-string)
+                      (expand-file-name (substring current-string 5))
+                    (if (not (string-match "^[a-z]+:" current-string))
+                        (expand-file-name current-string)
+                      current-string))))
+               (t (or (thing-at-point 'url) buffer-file-name))))
+        (program (if arg
+                     (read-shell-command "Open current file with: ")
+                   "xdg-open")))
     (call-process program nil 0 nil current-file-name)))
 
 
